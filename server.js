22e23f95b3151041969ed9a517497d11389a07b5
akornmann123 <123998259+akornmann123@users.noreply.github.com>
2023-02-15 20:48:57 -0600

    implement newFolderName

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const newFolderName = function (folders) {
+  const index = folders.reduce((index, name) => {
+  if (name === 'New Folder') {
+      return Math.max(0, index);
+    } else {
+      const [, match] = name.match(/^New Folder \((\d+)\)$/) || [];
+      return match ? Math.max(Number(match), index) : index;
+    }
+  }, -1);
+
+  return `New Folder${index === -1 ? '' : ` (${index === 0 ? 2 : index + 1})`}`;
 
 }
+
+console.log(newFolderName(["New Folder", "New Folder (2)", "New Folder (3)"]));
+            
