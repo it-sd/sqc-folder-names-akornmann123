1f2f28d7bfac26524e93c09ab2658a6486e1fdc8
akornmann123 <123998259+akornmann123@users.noreply.github.com>
2023-03-07 21:35:37 -0600

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const newFolderName = function (folders) {
       return match ? Math.max(Number(match), index) : index
     }
   }, -1)
-  return `New Folder${index === -1 ? '' : ` (${index === 0 ? 2 : index + 1})`}`
+  return `New Folder${index === -1 ? '' : `(${index === 0 ? 2 : index + 1})`}`
 }
 
 console.log(newFolderName(['New Folder', 'New Folder (2)', 'New Folder (3)']))
